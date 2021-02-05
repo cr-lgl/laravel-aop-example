9c6f39e75f736d7c445537690268dae0b9cbab0e
Surtr <cherryred5959@gmail.com>
2021-02-06 02:04:12 +0900

    fix a bug where goaop tags are registered as duplicates as the number of tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,21 @@
 
 namespace Tests;
 
+use Closure;
+use Go\Core\AspectContainer;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    public function tearDown(): void
+    {
+        Closure::fromCallable(function () {
+            /** @noinspection PhpUndefinedFieldInspection */
+            $this->tags = [];
+        })->call(resolve(AspectContainer::class));
+
+        parent::tearDown();
+    }
 }
